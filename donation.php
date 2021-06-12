d895d70d75afe26e15b97fc0f03a7967c3eab7bf
AdityaNaidu2k2 <pokala2002@gmail.com>
2021-06-12 15:15:10 +0530

    Added Baseline files

---
+++ donation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Donate Now</title>
+  <!-- Bootstrap -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+<!-- CSS Stylesheet -->
+  <link rel="stylesheet" href="css/donation.css">
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <h1>"Giving is not just about making a donation,its about making a <span class="difference">difference</span>"</h1>
+<!-- Code for the razorpay payment gateway -->
+  <form class="payment">
+    <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HM3LtLvBUV58B1" async>
+    </script>
+  </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sparks Foundation</title>
+  <!-- Bootstrap -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="container-fluid">
+    <!-- Nav Bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-light>">
+      <a class="navbar-brand" href="./index.php">Sparks Foundation</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./donation.php">Donate Now</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <h1>COVID 19</h1>
+  <p>A virus that needs no introduction. We are in the midst of one the deadliest crisis a man has ever faced.</p>
+  <p class="info1">In these tough times, Spark's Foundation have stepped in to provide relief to thousands of people who are suffering in this misery.</p>
+  <p class="info2">Help the nation in Flattening the Curve by donating to us.</p>
+<!-- Donation Button -->
+  <a href="./donation.php" class="donatebtn btn btn-info" role="button">DONATE NOW</a>
+  </div>
+
+</body>
+
+</html>
